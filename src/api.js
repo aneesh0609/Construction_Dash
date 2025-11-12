172aaad99829e9dffcf39e98c22b469da365fb52
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-11-12 21:49:39 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // e.g. http://localhost:5000/api
+  baseURL: import.meta.env.VITE_API_URL + "/api", // e.g. http://localhost:5000/api
   withCredentials: true // VERY IMPORTANT for cookie auth
 });
 
